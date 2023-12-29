51e75c0c8ae5e4a292f8cc905577281dc31015a1
Lourance <2455067339@qq.com>
2023-12-29 13:40:21 +0800

    初始化

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
+
+// 阶段性预警就部分开发和联调，首页疾病筛查部分开发和联调，监测数据部分开发和完善
+
+// banner图形完善开发，检测服务部分开发和联挑，预警部分开发和联调
