6938a1e411b2b09cf18879c2451f66249c33fb2f
Lourance <2455067339@qq.com>
2024-02-02 11:44:31 +0800

    -t

--- src/App.tsx
+++ src/App.tsx
@@ -9,14 +9,8 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+       我是进入的首页
       </div>
-      <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
